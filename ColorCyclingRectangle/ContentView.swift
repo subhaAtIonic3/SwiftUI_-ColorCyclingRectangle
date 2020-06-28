db33f53afef0ef0a7b7bbbd298cc00347a6a374c
Subhrajyoti <subha.ionic3@gmail.com>
2020-06-29 00:36:23 +0530

    Initial commit

--- ColorCyclingRectangle/ContentView.swift
+++ ColorCyclingRectangle/ContentView.swift
@@ -9,13 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var colorCycle = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ColorCyclingRectangle(amount: colorCycle)
+                .frame(width: 400, height: 300)
+            
+            Slider(value: $colorCycle)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorCyclingRectangle: View {
+    
+    var amount = 0.0
+    var steps = 100
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: CGFloat(value))
+                    //                    .strokeBorder(self.color(for: value, brightness: 1.0), lineWidth: 2)
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [self.color(for: value, brightness: 1), self.color(for: value, brightness: 0.5)]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
     }
 }
